da53e546f65defc93c82bd539397c18cc8f8c2c9
AlisonOoi123 <131657490+AlisonOoi123@users.noreply.github.com>
2024-09-12 21:26:31 +0800

    Update 4.Restaurant.py

--- pages/4.Restaurant.py
+++ pages/4.Restaurant.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
-from bokeh.models.widgets import Div
 from PIL import Image
+import os
 
 st.set_page_config(layout='centered', initial_sidebar_state='expanded')
 st.sidebar.image('Data/App_icon.png')
@@ -21,19 +21,13 @@ New_York = pd.read_csv('Data/New York/New_York.csv', sep=',')
 New_York["Location"] = New_York["Street Address"] +', '+ New_York["Location"]
 New_York = New_York.drop(['Street Address', ], axis=1)
 
-
-
 New_Jersey = pd.read_csv('Data/New Jersey/New_Jersey.csv', sep=',')
 New_Jersey["Location"] = New_Jersey["Street Address"] +', '+ New_Jersey["Location"]
 New_Jersey  = New_Jersey.drop(['Street Address', ], axis=1)
 
-
-
 Texas = pd.read_csv('Data/Texas/Texas.csv', sep=',')
 Texas["Location"] = Texas["Street Address"] +', '+ Texas["Location"]
 Texas = Texas.drop(['Street Address', ],axis=1)
-
-
 
 Washington = pd.read_csv('Data/Washington/Washington.csv', sep=',')
 Washington["Location"] = Washington["Street Address"] +', '+ Washington["Location"]
@@ -41,11 +35,7 @@ Washington = Washington.drop(['Street Address', ], axis=1)
 
 option = st.selectbox('Select Your State', ('New York','New Jersey','California', 'Texas', 'Washington'))
 
-
-
-#Details of every resturant
-def details(dataframe,option):
-
+def details(dataframe, option):
     dataframe = dataframe.drop(["Unnamed: 0", "Trip_advisor Url", "Menu"], axis=1)
     data_new = dataframe
 
@@ -76,7 +66,6 @@ def details(dataframe,option):
     new_df = pd.DataFrame(data1)
     data_new["No of Reviews"] = new_df[0]
     data_new["No of Reviews"] = data_new["No of Reviews"].astype(float)
-    
 
     if option == 'New Jersey':
         image = Image.open('Data/New Jersey/nj.png')
@@ -104,21 +93,16 @@ def details(dataframe,option):
         Reviews = (dataframe.at[dataframe['Name'].eq(title).idxmax(), 'Reviews'])
         st.subheader("Restaurant Rating:-")
 
-        #REVIEWS
+        # REVIEWS
         if Reviews == '4.5':
             image = Image.open('Data/Ratings/Img4.5.png')
             st.image(image, use_column_width=True)
-
-
         elif Reviews == '4':
             image = Image.open('Data/Ratings/Img4.0.png')
             st.image(image, use_column_width=True)
-
-
         elif Reviews == '5':
             image = Image.open('Data/Ratings/Img5.0.png')
             st.image(image, use_column_width=True)
-
         else:
             pass
 
@@ -128,38 +112,31 @@ def details(dataframe,option):
             comment = (dataframe.at[dataframe['Name'].eq(title).idxmax(), 'Comments'])
             if comment != "No Comments":
                 st.subheader("Comments:-")
-
                 st.warning(comment)
             else:
                 pass
 
-        #TYPE OF RESTURANT
+        # TYPE OF RESTAURANT
         Type = (dataframe.at[dataframe['Name'].eq(title).idxmax(), 'Type'])
         st.subheader("Restaurant Category:-")
         st.error(Type)
 
-        #LOCATION
+        # LOCATION
         Location = (dataframe.at[dataframe['Name'].eq(title).idxmax(), 'Location'])
         st.subheader("The Address:-")
-
         st.success(Location)
 
-        #CONTACT DETAILS
+        # CONTACT DETAILS
         contact_no = (dataframe.at[dataframe['Name'].eq(title).idxmax(), 'Contact Number'])
         if contact_no == "Not Available":
             pass
-
         else:
             st.subheader("Contact Details:-")
             st.info('Phone:- '+ contact_no)
 
-        
     st.text("")
     image = Image.open('Data/food_2.jpg')
     st.image(image, use_column_width=True)
-
-
-
 
 if option == 'New Jersey':
     details(New_Jersey, option)
@@ -176,3 +153,28 @@ elif option == 'Texas':
 elif option == 'Washington':
     details(Washington,option)
 
+# Collect User Feedback
+st.markdown("## Rate Your Experience")
+rating = st.slider('Rate this restaurant (1-5)', 1, 5)
+feedback_comment = st.text_area('Your Feedback')
+
+if st.button('Submit Feedback'):
+    feedback_file = 'Data/feedback.csv'
+
+    # Create the CSV file if it doesn't exist
+    if not os.path.isfile(feedback_file):
+        feedback_df = pd.DataFrame(columns=['Reviews', 'Comments'])
+        feedback_df.to_csv(feedback_file, index=False)
+
+    # Load existing feedback data
+    feedback_df = pd.read_csv(feedback_file)
+
+    # Append new feedback
+    new_feedback = pd.DataFrame([{'Reviews': f'{rating} of 5 bubbles', 'Comments': feedback_comment}])
+    feedback_df = pd.concat([feedback_df, new_feedback], ignore_index=True)
+    feedback_df.to_csv(feedback_file, index=False)
+    
+    st.success('Thanks for your feedback!')
+
+    # Clear the feedback form
+    st.experimental_rerun()
