4a9cdc9c2867ac098598494e543f0cbd65e9aacd
AlisonOoi123 <131657490+AlisonOoi123@users.noreply.github.com>
2024-09-13 12:50:00 +0800

    Update 5.Location.py

--- pages/5.Location.py
+++ pages/5.Location.py
@@ -67,9 +67,9 @@ st.markdown(
     <label style='font-family:"Comic Sans MS", cursive; font-size:18px;'>
     Enter a location:
     </label>
-    <p> Example: 942, 21st Main Road, 2nd Stage, Banashankari, Bangalore
+     <p style='font-size:12px;'> Example: 942, 21st Main Road, 2nd Stage, Banashankari, Bangalore
     </p>
-     <p> Example: 33, 15th Cross, 7th Main, 2nd Stage, BTM, Bangalore
+    <p style='font-size:12px;'> Example: 33, 15th Cross, 7th Main, 2nd Stage, BTM, Bangalore
     </p>
     """, 
     unsafe_allow_html=True
