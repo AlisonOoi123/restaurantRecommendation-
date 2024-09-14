3bdb3a9d1038d2acf5587ce234d5821f5af8581b
AlisonOoi123 <131657490+AlisonOoi123@users.noreply.github.com>
2024-09-14 20:19:47 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -14,5 +14,4 @@ st.title("Discover the best places to eat in your town.")
 
 st.markdown("Powered by data from TripAdvisor, our app curates recommendations from 20 cities across New York, New Jersey, California, Texas, and Washington. Find the top 10 restaurants similar to your favorites.")
 st.markdown("Leveraging Natural Language Processing and Content-Based Recommender Systems, we prioritize user comments to deliver personalized suggestions.")
-st.markdown("Ready to explore? [Click here to view our recommendations!](Recommended)")
 st.success("Satisfy your cravings with ease! :fork_and_knife:" ":yum:")
